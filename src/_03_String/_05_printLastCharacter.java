7e7b52ed2b73746e031dc73dd6ca1911c6f439c3
esalkan <esalkan@icloud.com>
2021-10-27 17:09:49 +0300

    String Manipulations - Print Last Character

---
+++ src/_03_String/_05_printLastCharacter.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package _03_String;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _05_printLastCharacter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		/**
+		 * Write a program that will print out last letter of the word (string).
+		 * 
+		 * Ex:
+		 * Input: java 
+		 * Output: a
+		 */
+
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+		System.out.println(word.charAt(word.length() - 1));
+
+	}
+
+}
