855af3cbbe9a0a79c628d61bd1cf4045e10c93f0
esalkan <esalkan@icloud.com>
2021-10-28 09:58:32 +0300

    Lab 3 - Print First And Last Letters

---
+++ src/_03_String/_06_printFirstAndLastLetters.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package _03_String;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _06_printFirstAndLastLetters {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		/**
+		 * Write a program that will print out first and last letters together.
+		 * 
+		 * Input: adobe Output: ae
+		 */
+
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+
+		System.out.print(word.charAt(0));
+		System.out.println(word.charAt(word.length() - 1));
+	}
+
+}
