43d6c25f77c867d447012ef2459ce6fe49a33630
esalkan <esalkan@icloud.com>
2021-10-28 11:48:09 +0300

    Lab 3 - Question 09

---
+++ src/_03_String/_09_mergeThem.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package _03_String;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _09_mergeThem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * You have 2 words, both of them have 3 characters.
+		 * 
+		 * If either of them does not have exactly 3 characters, print "cannot merge"
+		 * 
+		 * Merge their characters one by one and print together like below:
+		 * 
+		 * Input: aok lol Output: alookl
+		 * 
+		 * Input: aok lol Output: alookl
+		 * 
+		 * Input: ear pie Output: epaire
+		 * 
+		 * Input: ear pie Output: epaire
+		 * 
+		 * Input: java wow Output: cannot merge
+		 * 
+		 * Input: java wow Output: cannot merge
+		 * 
+		 * hint: by inserting +""+ (empty string) between chars, you can concatenate char
+		 * values.
+		 */
+		
+	    Scanner scan = new Scanner(System.in);
+	    String word1 = scan.next();
+	    String word2 = scan.next();
+	    
+	    if(word1.length() != 3 || word2.length() != 3) {
+	    	System.out.println("cannot merge");
+	    } else {
+	    	for(int i = 0; i < 3; i++) {
+	    		System.out.print(word1.charAt(i) + "" + word2.charAt(i));
+	    	}
+	    }
+	}
+}
