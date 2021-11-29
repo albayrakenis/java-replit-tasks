1813fa46183bd0f16c701f3939d893c07ead7333
esalkan <esalkan@icloud.com>
2021-11-29 10:24:55 +0300

    Create RemoveEveryOthers.java

---
+++ src/_07_ArrayList/RemoveEveryOthers.java
@@ -0,0 +1,32 @@
+package _07_ArrayList;
+
+/**
+ * @author esalkan
+ *
+ */
+public class RemoveEveryOthers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Create a method that:
+		 * 
+		 * is called removeEveryOther returns ArrayList of Strings takes in a single
+		 * parameter - an ArrayList of Strings called words This method should take the
+		 * ArrayList parameter and modify it by removing every other element in the
+		 * list, starting with removing the 0th element.
+		 * 
+		 * For example, if the parameter is:
+		 * 
+		 * ("hi","yo","sup","yolo","book")
+		 * 
+		 * The modified ArrayList should be:
+		 * 
+		 * ("yo","yolo")
+		 */
+
+	}
+
+}
